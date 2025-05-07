12e737107c6135ea8f15e50eea2dad142928ba06
Daniel Castro <dcastro@ualberta.ca>
2025-05-07 16:00:25 -0600

    chore: remove extra merge button

--- lib/ui/bottom_bar.dart
+++ lib/ui/bottom_bar.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:fluent_ui/fluent_ui.dart' as fluent;
 import '../platform/platform_widgets.dart';
 
 class BottomBar extends StatelessWidget {
-  final VoidCallback onMergeAll;
-  final VoidCallback onMergeSelected;
+  final VoidCallback onMerge;
 
-  const BottomBar({
-    super.key,
-    required this.onMergeAll,
-    required this.onMergeSelected,
-  });
+  const BottomBar({super.key, required this.onMerge});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
       padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          PlatformButton(
-            onPressed: onMergeSelected,
-            child: Text("Merge Selected"),
-          ),
-          PlatformButton(onPressed: onMergeAll, child: Text("Merge All")),
-        ],
+        children: [PlatformButton(onPressed: onMerge, child: Text("Merge"))],
       ),
     );
   }
